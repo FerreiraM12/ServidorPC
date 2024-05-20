9c314f37cdf0bc1d51e26e1098dac34268407fea
marlonferreira <marlon.daniel.df@gmail.com>
2024-05-20 17:18:40 +0100

    multiple clients support

--- client_handler.erl
+++ client_handler.erl
@@ -7,8 +7,15 @@
 -record(player, {id, x, y, direction}).  
 
 start(Port) ->
+    init_ets_table(),
     {ok, ListenSocket} = gen_tcp:listen(Port, [{active, false}, {packet, 0}, {reuseaddr, true}]),
     loop(ListenSocket).
+
+init_ets_table() ->
+    case ets:info(client_sockets) of
+        undefined -> ets:new(client_sockets, [named_table, set, public]);
+        _ -> ok
+    end.
 
 loop(ListenSocket) ->
     {ok, Socket} = gen_tcp:accept(ListenSocket),
@@ -17,9 +24,17 @@ loop(ListenSocket) ->
 
 
 initialize_player(Socket) -> 
-    NewPlayer = #player{id = 1, x = 500, y = 500, direction = 0},
+    PlayerId = next_player_id(),
+    NewPlayer = #player{id = PlayerId, x = 500, y = 500, direction = 0},
+    ets:insert(client_sockets, {Socket, NewPlayer}),
     io:format("New client connected. Player ID: ~p~n", [NewPlayer#player.id]),  % Debug print
     handle_client(Socket, NewPlayer).
+
+next_player_id() ->
+    case ets:info(client_sockets) of
+        undefined -> 1;
+        _ -> ets:info(client_sockets, size) + 1
+    end.
 
 handle_client(Socket, Player) ->
     case gen_tcp:recv(Socket, 0) of
@@ -30,6 +45,7 @@ handle_client(Socket, Player) ->
             handle_client(Socket, NewPlayer);
         {error, Reason} ->
             io:format("Error reading data from client: ~p~n", [Reason]),  % Debug print
+            ets:delete(client_sockets, Socket),
             gen_tcp:send(Socket, "Erro: Não foi possível ler os dados\n"),
             gen_tcp:close(Socket)
     end.
@@ -66,26 +82,34 @@ process_command(Socket, NetData, Player) ->
         "a" ->
             %% Turn the player left
             NewPlayer = movement:turn_left(Player),
+            update_player(Socket, NewPlayer),
             NewPlayer;
         "d" ->
             %% Turn the player right
             NewPlayer = movement:turn_right(Player),
+            update_player(Socket, NewPlayer),
             NewPlayer;
         "w" ->
             %% Move the player forward
             NewPlayer = movement:move_forward(Player),
+            update_player(Socket, NewPlayer),
             NewPlayer;
         "q" ->
             gen_tcp:send(Socket, "Quitting game\n"),
+            ets:delete(client_sockets, Socket),
             gen_tcp:close(Socket);
         _ ->
             gen_tcp:send(Socket, "Invalid command\n")
     end.
 
-send_updated_position(Socket, Player) ->
-    %% Send updated position to the client
-    io:format("updated position: ~p,~p,~p", [Player#player.x, Player#player.y, Player#player.direction]),  % Debug print
+update_player(Socket, Player) ->
+    ets:insert(client_sockets, {Socket, Player}).
+
+send_updated_position(_, Player) ->
+    %% Broadcast updated position to all clients
     UpdatedPosition = io_lib:format("player_coords ~p ~p ~p\n", [Player#player.id, Player#player.x, Player#player.y]),
-    gen_tcp:send(Socket, UpdatedPosition).
-
-
+    io:format("Broadcasting updated position: ~p~n", [UpdatedPosition]),  % Debug print
+    AllClients = ets:tab2list(client_sockets),
+    lists:foreach(fun({ClientSocket, _}) -> 
+        gen_tcp:send(ClientSocket, UpdatedPosition)
+    end, AllClients).
