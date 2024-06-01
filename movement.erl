b0c572d911af236397af62e7e5c3fe6745ac0b02
miguelrego25 <61994815+miguelrego25@users.noreply.github.com>
2024-06-01 12:13:14 +0100

    Update with velocity and continuos movement.

--- movement.erl
+++ movement.erl
@@ -7,24 +7,32 @@
 
 -export([move_forward/1, turn_left/1, turn_right/1]).
 
--record(player, {id, socket, x, y, direction, level = 1, locked = false, consecutive_wins = 0, consecutive_losses = 0, gamePid = 0}).
+-record(player, {id, socket, x, y, direction, velocity = {0,0}, level = 1, locked = false, consecutive_wins = 0, consecutive_losses = 0, gamePid = 0}).
 
 to_radians(Degrees) ->
     Degrees * math:pi() / 180.
 
 %% Move the player forward in the direction they are facing
+
 move_forward(Player) ->
-  XDelta = math:cos(to_radians(math:floor(Player#player.direction))),
-  YDelta = math:sin(to_radians(math:floor(Player#player.direction))),
-  NewX = Player#player.x + XDelta * 10,
-  NewY = Player#player.y + YDelta * 10,
-  Player#player{x = NewX, y = NewY}.
+  {Vx, Vy} = Player#player.velocity,
+  NNewx = Vx + 1,
+  NNewy = Vy + 1,
+  XDelta = math:cos(to_radians(Player#player.direction))*NNewx,
+  YDelta = math:sin(to_radians(Player#player.direction))*NNewy,
+  NewVelocity = {NNewx, NNewy},
+  %% Update position based on new velocity
+  NewX = Player#player.x + XDelta,
+  NewY = Player#player.y + YDelta,
+  io:format("Moving player ~p to ~p, ~p with velocity ~p~n", [Player#player.id, NewX, NewY, NewVelocity]),
+  Player#player{velocity = NewVelocity, x = NewX, y = NewY}.
+
 
 %% Turn the player to the left
 turn_left(Player) ->
-  NewDirection = (Player#player.direction - 90 + 360) rem 360,  % Handle negative values
+  NewDirection = (Player#player.direction - 15 + 360) rem 360,  % Handle negative values
   Player#player{direction = NewDirection}.
 
 turn_right(Player) ->
-  NewDirection = (Player#player.direction + 90) rem 360,
+  NewDirection = (Player#player.direction + 15) rem 360,
   Player#player{direction = NewDirection}.
